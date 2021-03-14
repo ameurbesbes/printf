896dbd1d4b2e301775edf38d6e2ec7e7d6497252
ameurbesbes <ameurbesbes@outlook.com>
2021-03-14 18:06:32 +0100

    printS.c

--- printS.c
+++ printS.c
@@ -8,7 +8,7 @@ int printstring(va_list va)
 {
         int i; 
 	char *tmp = va_arg(va, char *);
-	for(i = 0; tmp[i] != '\0', i++);
+	for(i = 0; tmp[i] != '\0'; i++);
 	_putchar(tmp[i]);
         return(i + 1);
 }
