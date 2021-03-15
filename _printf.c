faffe764f96b639462a9a0dea1627c9940d65dc9
ElAmirr <othmaniamir41@gmail.com>
2021-03-15 11:59:19 +0100

    update the printf file

---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ *_printf- function that acts like printf
+ *@format: format
+ *Return: return 1 or -1 and l
+ */
+int _printf(const char *format, ...)
+{
+	va_list va;
+	int i, j, l = 0;
+s_t func[] = {
+{"c", print_char},
+{"s", print_string},
+{"d", print_integer},
+{NULL, NULL},
+};
+if (format == NULL)
+return (-1);
+va_start(va, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '\0')
+return (-1);
+if (format[i] == '%')
+{
+_putchar(format[i]);
+l++;
+}
+for (j = 0; (func + j)->c != NULL; j++)
+{
+if (format[i] == *(func + j)->c)
+l += (func + j)->f(va);
+}
+}
+else
+{
+_putchar(format[i]);
+l++;
+}
+}
+va_end(va);
+return (l);
+}
